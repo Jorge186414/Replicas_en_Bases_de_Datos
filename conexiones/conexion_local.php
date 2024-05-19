06e029ce1270e616a9d324d90d68a8982f7b57cb
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-05-19 03:06:21 -0600

    Delete: Eliminacion de mensaje de conexion exitosa a la BD

--- conexiones/conexion_local.php
+++ conexiones/conexion_local.php
@@ -3,7 +3,7 @@
     $servidor = "localhost";
     $usuario = "jorge";
     $contrasenia = "luffy1464";
-    $base_de_datos = "to_do_list";
+    $base_de_datos = "lista_de_tareas";
 
     # Conexion al servidor de MysSQL
     $conexion = mysqli_connect($servidor, $usuario, $contrasenia, $base_de_datos);
@@ -11,6 +11,5 @@
     if(!$conexion){
         echo "Problemas al conectar con la base de datos";
     }else{
-        echo "Conexion exitosa";
     }
 ?>
