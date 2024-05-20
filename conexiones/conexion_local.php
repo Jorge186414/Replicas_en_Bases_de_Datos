01b62887692c4e710372f62f92f1cf44c2834431
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-05-20 01:10:42 -0600

    Modified: cambio del servidor principal

--- conexiones/conexion_local.php
+++ conexiones/conexion_local.php
@@ -1,12 +1,12 @@
 <?php
     # Datos del servidor local de MySQL
-    $servidorP = "localhost";
+    $servidorP = "127.0.0.1";
     $usuarioP = "root";
     $contraseniaP = "1464";
     $base_de_datosP = "lista_de_tareas";
 
     # Datos del servidor remoto de MySQL
-    $servidorS = "ip_remota";
+    $servidorS = "direccion_ip_remota";
     $usuarioS = "servidorRemoto";
     $contraseniaS = "1234";
     $base_de_datosS = "lista_de_tareas";
@@ -20,12 +20,10 @@
         
         if ($conexion) {
             $_SESSION['host'] = $servidorS;
-            echo "Conectado desde el servidor remoto: " . $_SESSION['host'];
         } else {
             echo "Error en la conexión remota: " . mysqli_connect_error();
         }
     } else {
         $_SESSION['host'] = $servidorP;
-        echo "Conectado desde el servidor local: " . $_SESSION['host'];
     }
 ?>
